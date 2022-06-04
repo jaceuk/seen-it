5263cb6ccdb11a6d1d475a8a15cb44b44fd1c26a
jaceuk <jnewington@gmail.com>
2022-06-04 22:18:19 +0100

    Search setup

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let searchValue: string;
+  import { goto } from '$app/navigation';
+
+  function handleSubmit() {
+    goto('/search/' + searchValue);
+  }
+</script>
+
+<form>
+  <label for="search">Search</label>
+  <input id="search" type="text" bind:value={searchValue} />
+  <button type="submit" on:click|preventDefault={handleSubmit}>Search</button>
+</form>
---
+++ src/routes/search/[name].svelte
@@ -0,0 +1,45 @@
+<script context="module" lang="ts">
+  interface ILoad {
+    fetch: any;
+    params: any;
+  }
+
+  export async function load({ fetch, params }: ILoad) {
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_TMDB_API_KEY}&query=${params.name}`,
+      );
+      const data = await res.json();
+      if (res.ok) {
+        return {
+          status: 200,
+          props: { results: data.results },
+        };
+      } else {
+        return {
+          status: 400,
+          props: { error: data.status_message },
+        };
+      }
+    } catch (error) {
+      console.error(error);
+      return {
+        status: 500,
+        props: { error: error },
+      };
+    }
+  }
+</script>
+
+<script lang="ts">
+  export let results: any;
+  export let error: any;
+</script>
+
+{#if error}
+  <p>{error}</p>
+{:else}
+  {#each results as result}
+    <p>{result.title}</p>
+  {/each}
+{/if}
